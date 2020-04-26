4c49d517fab5d702a0f6bcfe5ec221696701490d
Heitor Quaglia <heitor.bq@hotmail.com>
2020-04-25 22:59:53 -0300

    Construct

--- Deque/deque.h
+++ Deque/deque.h
@@ -1,4 +1,19 @@
 #pragma once
+
+template <typename T>
+class Node {
+private:
+	T data;
+	Node<T>* previous;
+	Node<T>* next;
+public:
+	Node(T data) {
+		this->data = data;
+		this->previous = nullptr;
+		this->next = nullptr;
+	}
+};
+template <typename T>
 class Deque
 {
 
