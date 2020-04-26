04d7d00b70ba396545e1002529077ad24b9c34b6
Heitor Quaglia <heitor.bq@hotmail.com>
2020-04-25 23:02:29 -0300

    encapsulamento Node

--- Deque/deque.h
+++ Deque/deque.h
@@ -12,6 +12,18 @@ public:
 		this->previous = nullptr;
 		this->next = nullptr;
 	}
+	Node<T>* get_previous() {
+		return this->previous;
+	}
+	void set_previous(Node<T>* previous) {
+		this->previous = previous;
+	}
+	Node<T>* get_next() {
+		return this->next;
+	}
+	void set_next(Node<T>* next) {
+		this->next = next;
+	}
 };
 template <typename T>
 class Deque
