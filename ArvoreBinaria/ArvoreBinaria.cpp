621228a9a905d696a9fdd793332b27e3677dfd66
Heitor Quaglia <heitor.bq@hotmail.com>
2020-06-05 00:12:02 -0300

    test tree

--- ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "Tree.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Tree *tree = new Tree();
+
+	tree->insert(8);
+	tree->insert(10);
+	tree->insert(14);
+	tree->insert(13);
+
+	tree->insert(3);
+	tree->insert(1);
+	tree->insert(6);
+	tree->insert(4);
+	tree->insert(7);
+
+	std::cout << "Em ordem: " << std::endl;
+
+	tree->printTree(tree->get_root());
 }
